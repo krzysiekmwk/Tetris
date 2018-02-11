8a15789c69226079a1e0a28535511f666a976755
krzysiekmwk <krzysiek.mwk@gmail.com>
2018-02-12 00:59:54 +0100

    Added S-Block

---
+++ app/src/main/java/pl/knowakowski/tetris/SBlock.java
@@ -0,0 +1,28 @@
+package pl.knowakowski.tetris;
+
+import android.graphics.Paint;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by krzysiek on 12.02.2018.
+ */
+
+public class SBlock extends Figure {
+
+    SBlock(ArrayList<Block> gameBoard){
+        this.gameBoard = gameBoard;
+        blocksContainer = new ArrayList<>();
+
+        Paint color = new Paint();
+        color.setARGB(255,0,255,0);//Green
+
+        blocksContainer.add(new Block(4,4,color));
+        blocksContainer.add(new Block(5,4,color));
+        blocksContainer.add(new Block(5,5,color));
+        blocksContainer.add(new Block(6,5,color));
+
+        Collections.sort(blocksContainer);
+    }
+}
