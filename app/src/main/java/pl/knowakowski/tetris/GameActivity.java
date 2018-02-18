e311b05455d51bebf00e828414c3e41574a509f0
krzysiekmwk <krzysiek.mwk@gmail.com>
2018-02-18 14:32:31 +0100

    added another surfaced view to show communicates

--- app/src/main/java/pl/knowakowski/tetris/GameActivity.java
+++ app/src/main/java/pl/knowakowski/tetris/GameActivity.java
@@ -3,6 +3,7 @@ package pl.knowakowski.tetris;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 public class GameActivity extends Activity implements Callback {
 
     private GameSurfaceView gameSurfaceView;
+    private SurfaceView topSurfaceView;
 
     private Button buttonLeft;
     private Button buttonRight;
@@ -24,8 +26,11 @@ public class GameActivity extends Activity implements Callback {
         setContentView(R.layout.activity_game);
 
         gameSurfaceView = findViewById(R.id.surfaceView_gamePanel);
-        SurfaceHolder surfaceHolder = gameSurfaceView.getHolder();
-        surfaceHolder.addCallback(gameSurfaceView);
+        SurfaceHolder surfaceHolderGameSurfaceView = gameSurfaceView.getHolder();
+        surfaceHolderGameSurfaceView.addCallback(gameSurfaceView);
+
+        topSurfaceView = findViewById(R.id.surfaceView_aboveGameSurfaceView);
+        topSurfaceView.setZOrderOnTop(true);
 
         buttonLeft = findViewById(R.id.button_left);
         buttonRight = findViewById(R.id.button_right);
